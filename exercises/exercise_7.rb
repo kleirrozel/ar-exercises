3a3745619d4af8d2e4871413caee44761b0056a1
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-16 17:54:16 -0400

    Validations and creating new store name done

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# 2
+puts "Enter name of store: "
+@store_name = gets.chomp
+
+# 3
+@new_store = Store.create(name: @store_name)
+
+# 4
+puts @new_store.errors.messages
