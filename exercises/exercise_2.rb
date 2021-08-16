3d9059bc0e4565346c8d9e0b80bfdfb8f3b6ab13
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-16 15:30:33 -0400

    Changed store name with id 1 from Burnaby to Bunnerville

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts @store1.name
+puts "----------"
+@store1.name = "Bunnerville"
+@store1.save
+puts "----------"
+puts @store1.name
