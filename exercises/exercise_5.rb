c297feba447194fb0a2eb5800bdc893e4ee02b37
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-16 16:51:27 -0400

    One to Many! Don't skip steps!

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Total revenue for the entire company (all stores)
+@total_revenue = Store.sum("annual_revenue")
+
+# Average annual revenue
+@average_revenue = Store.average("annual_revenue")
+
+# Number of stores generating $1M or more in annual sales
+@high_earning_stores = Store.where("annual_revenue > 1000000").count
+
+puts "Total Revenue: #{@total_revenue}"
+puts "Average Revenue: #{@average_revenue}"
+puts "Stores generating $1M annually: #{@high_earning_stores}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Elvis", last_name: "Bun", hourly_rate: 100)
+@store1.employees.create(first_name: "Betty", last_name: "Folklore", hourly_rate: 75)
+@store1.employees.create(first_name: "Dorothea", last_name: "Evermore", hourly_rate: 75)
+@store1.employees.create(first_name: "Grogu", last_name: "Yoda", hourly_rate: 25)
+@store1.employees.create(first_name: "Mando", last_name: "Lorian", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Olive", last_name: "Bean", hourly_rate: 85)
+@store2.employees.create(first_name: "Eli", last_name: "Bunner", hourly_rate: 15)
+@store2.employees.create(first_name: "Rocky", last_name: "Pom", hourly_rate: 20)
+@store2.employees.create(first_name: "Sachi", last_name: "Poo", hourly_rate: 45)
+
